6c2f36c55062994fd72b83630905a6b3c36abd57
Andrei Goreglyad <asgoreglyad@gmail.com>
2022-06-17 15:51:42 +0200

    intentional error

--- ConsoleAppForTesting/Program.cs
+++ ConsoleAppForTesting/Program.cs
@@ -15,6 +15,7 @@ namespace ConsoleAppForTesting
             string input = string.Empty;
             while (input != "q")
             {
+                int i = 1.545;
                 Console.WriteLine("Type a number, please");
                 input = Console.ReadLine();
                 if (int.TryParse(input, out int number))
